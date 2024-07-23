0b51b2cb1c1525ab2e799d657b787d7844375c68
Faizan Ansari <81568950+Fzansa@users.noreply.github.com>
2024-07-23 23:09:56 +0530

    cra & tailwind config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-3xl text-red-500 font-bold text-center my-32">
+      Tere bin ab na lenge ek v dam
+    </div>
+  );
+}
+
+export default App;
